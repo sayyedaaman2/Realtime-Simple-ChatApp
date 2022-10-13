3a03e4a191114aab34b409164d3ccf11d6f423c4
sayyedaaman2 <sayyedaaman9@gmail.com>
2022-10-13 17:00:53 +0530

    adding server side

---
+++ server/server.js
@@ -0,0 +1,48 @@
+const { instrument } = require('@socket.io/admin-ui')
+const io = require("socket.io")(3000, {
+    cors : {
+        origin : ['http://localhost:8080', 'https://admin.socket.io']
+    }
+});
+
+const userIo = io.of('/user')
+userIo.on('connection', socket =>{
+    console.log('connected to user namespcae with username ' + socket.username);
+});
+
+userIo.use((socket, next)=>{
+    if(socket.handshake.auth.token){
+        socket.username = getUsernamefromToken(socket.handshake.auth.token)
+        next();
+    }
+    else{
+        next(new Error ('Please send token'));
+    }
+});
+
+function getUsernamefromToken(token){
+    return token;
+}
+io.on('connection', socket =>{
+    console.log(socket.id);
+    // socket.on('custom-event', (number, string, obj)=>{
+    //     console.log(number, string, obj);
+    // })
+    socket.on('send-message', (message, room)=>{
+        if(room  === ''){
+            socket.broadcast.emit('receive-message', message);
+        // console.log(message);
+        }else{
+            socket.to(room).emit('receive-message', message);
+
+        }
+        
+    })
+    socket.on('join-room', (room, cb) =>{
+        socket.join(room);
+        cb(`Joined ${room}`);
+    })
+    socket.on ('ping', n => console.log(n));
+})
+
+instrument(io, {auth : false});
